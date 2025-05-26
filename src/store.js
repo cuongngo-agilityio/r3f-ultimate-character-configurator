adc8259f5535445c9b11f3b27f98ea9ace051758
Cuong Ngo <cuongngo@asnet.com.vn>
2025-05-26 16:14:54 +0700

    Add customization management to configurator store

--- src/store.js
+++ src/store.js
@@ -13,6 +13,9 @@ export const useConfiguratorStore = create((set) => ({
   categories: [],
   currentCategory: null,
   assets: [],
+  customization: {},
+  download: () => {},
+  setDownload: (download) => set({ download }),
   fetchCategories: async () => {
     // you can also fetch all records at once via getFullList
     const categories = await pb.collection("CustomizationGroups").getFullList({
@@ -21,11 +24,28 @@ export const useConfiguratorStore = create((set) => ({
     const assets = await pb.collection("CustomizationAssets").getFullList({
       sort: "-created",
     });
+    const customization = {};
     categories.forEach((category) => {
       category.assets = assets.filter((asset) => asset.group === category.id);
+      customization[category.name] = {};
+      if (category.startingAsset) {
+        customization[category.name].asset = category.assets.find(
+          (asset) => asset.id === category.startingAsset
+        );
+      }
     });
 
-    set({ categories, currentCategory: categories[0], assets });
+    set({ categories, currentCategory: categories[0], assets, customization });
   },
   setCurrentCategory: (category) => set({ currentCategory: category }),
+  changeAsset: (category, asset) =>
+    set((state) => ({
+      customization: {
+        ...state.customization,
+        [category]: {
+          ...state.customization[category],
+          asset,
+        },
+      },
+    })),
 }));
