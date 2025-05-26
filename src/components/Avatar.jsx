116d89406314086f7622233f0dd7ca6aac93e13c
Cuong Ngo <cuongngo@asnet.com.vn>
2025-05-26 16:54:55 +0700

    Refactor Avatar component to integrate animations and enable GLTF export functionality

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,15 +1,50 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
+import { Suspense, useEffect, useRef } from "react";
+import { GLTFExporter } from "three-stdlib";
 import { pb, useConfiguratorStore } from "../store";
 import { Asset } from "./Asset";
-import { Suspense, useRef } from "react";
 
 export const Avatar = ({ ...props }) => {
   const group = useRef();
   const { nodes } = useGLTF("/models/Armature.glb");
+  const { animations } = useFBX("/models/Idle.fbx");
   const customization = useConfiguratorStore((state) => state.customization);
+  const { actions } = useAnimations(animations, group);
+  const setDownload = useConfiguratorStore((state) => state.setDownload);
 
-  console.log(nodes);
+  useEffect(() => {
+    function download() {
+      const exporter = new GLTFExporter();
+      exporter.parse(
+        group.current,
+        function (result) {
+          save(
+            new Blob([result], { type: "application/octet-stream" }),
+            `avatar_${+new Date()}.glb`
+          );
+        },
+        function (error) {
+          console.error(error);
+        },
+        { binary: true }
+      );
+    }
 
+    const link = document.createElement("a");
+    link.style.display = "none";
+    document.body.appendChild(link); // Firefox workaround, see #6594
+
+    function save(blob, filename) {
+      link.href = URL.createObjectURL(blob);
+      link.download = filename;
+      link.click();
+    }
+    setDownload(download);
+  }, []);
+
+  useEffect(() => {
+    actions["mixamo.com"]?.play();
+  }, [actions]);
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
@@ -20,7 +55,7 @@ export const Avatar = ({ ...props }) => {
               customization[key]?.asset?.url && (
                 <Suspense key={customization[key].asset.id}>
                   <Asset
-                    url={pb.files.getUrl(
+                    url={pb.files.getURL(
                       customization[key].asset,
                       customization[key].asset.url
                     )}
