56807ca85cc32c708ee9f92c5614667517d68481
Cuong Ngo <cuongngo@asnet.com.vn>
2025-05-26 10:20:20 +0700

    Fix URL method name in AssetsBox and adjust layout in UI component

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -31,7 +31,7 @@ const AssetsBox = () => {
             key={index}
             className={`w-20 h-20 rounded-md overflow-hidden bg-gray-200 pointer-events-auto hover:opacity-100 transition-all border-2 duration-500`}
           >
-            <img src={pb.files.getUrl(asset, asset.thumbnail)} />
+            <img src={pb.files.getURL(asset, asset.thumbnail)} />
           </button>
         ))}
       </div>
@@ -51,13 +51,7 @@ export const UI = () => {
   return (
     <main className="pointer-events-none fixed z-10 inset-0 p-10">
       <div className="mx-auto h-full max-w-screen-xl w-full flex flex-col justify-between">
-        <div className="flex justify-between items-center">
-          <a
-            className="pointer-events-auto"
-            href="https://lessons.wawasensei.dev/courses/react-three-fiber"
-          >
-            <img className="w-20" src="/images/wawasensei-white.png" />
-          </a>
+        <div className="flex justify-end items-center">
           <DownloadButton />
         </div>
         <div className="flex flex-col gap-6">
