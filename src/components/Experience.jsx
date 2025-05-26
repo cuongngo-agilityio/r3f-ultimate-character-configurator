a5ad6dfacc475753f83b9151325229e4ae884105
Cuong Ngo <cuongngo@asnet.com.vn>
2025-05-26 16:15:10 +0700

    Add Experience component to integrate Avatar and OrbitControls

---
+++ src/components/Experience.jsx
@@ -0,0 +1,11 @@
+import { Avatar } from "./Avatar";
+import { OrbitControls } from "@react-three/drei";
+
+export const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <Avatar />
+    </>
+  );
+};
