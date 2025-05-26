e8789f69e088d40d59cc00f02a0b09bb55d00164
Cuong Ngo <cuongngo@asnet.com.vn>
2025-05-26 10:20:15 +0700

    Refactor store.js to improve error message and export PocketBase instance

--- src/store.js
+++ src/store.js
@@ -1,30 +1,31 @@
 import { create } from "zustand";
+
 import PocketBase from "pocketbase";
 
-const pocketbaseUrl = import.meta.env.VITE_POCKETBASE_URL;
-if (!pocketbaseUrl) {
-  throw new Error("VITE_POCKETBASE_URL is not defined");
+const pocketBaseUrl = import.meta.env.VITE_POCKETBASE_URL;
+if (!pocketBaseUrl) {
+  throw new Error("VITE_POCKETBASE_URL is required");
 }
 
-const pb = new PocketBase(pocketbaseUrl);
+export const pb = new PocketBase(pocketBaseUrl);
 
-const useConfiguratorStore = create((set) => ({
-  // State
+export const useConfiguratorStore = create((set) => ({
   categories: [],
   currentCategory: null,
   assets: [],
   fetchCategories: async () => {
-    const categories = await pb.collection("categories").getFullList({
+    // you can also fetch all records at once via getFullList
+    const categories = await pb.collection("CustomizationGroups").getFullList({
       sort: "+position",
     });
-    const assets = await pb.collection("assets").getFullList({
+    const assets = await pb.collection("CustomizationAssets").getFullList({
       sort: "-created",
     });
+    categories.forEach((category) => {
+      category.assets = assets.filter((asset) => asset.group === category.id);
+    });
+
     set({ categories, currentCategory: categories[0], assets });
   },
-  setCurrentCategory: (category) => {
-    set({ currentCategory: category });
-  },
+  setCurrentCategory: (category) => set({ currentCategory: category }),
 }));
-
-export default useConfiguratorStore;
