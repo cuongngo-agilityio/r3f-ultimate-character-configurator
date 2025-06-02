68b1f3299c7e370d19a5a2c37f9b0b96aba94f1e
Cuong Ngo <cuongngo@asnet.com.vn>
2025-06-02 15:20:55 +0700

    feat: hide Leva UI and enable preserveDrawingBuffer for canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
 import { UI } from "./components/UI";
 import { Experience } from "./components/Experience";
 import { DEFAULT_CAMERA_POSITION } from "./components/CameraManager";
@@ -6,11 +7,15 @@ import { DEFAULT_CAMERA_POSITION } from "./components/CameraManager";
 function App() {
   return (
     <>
+      <Leva hidden />
       <UI />
       <Canvas
         camera={{
           position: DEFAULT_CAMERA_POSITION,
           fov: 45,
+        }}
+        gl={{
+          preserveDrawingBuffer: true,
         }}
         shadows
       >
